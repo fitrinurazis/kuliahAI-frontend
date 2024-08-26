beaaf5cbc0e20ab8b3c64cbb0cb0b080955f7612
unknown <Irfansyamsul180@gmail.com>
2024-08-26 22:20:27 +0700

    feat/prody

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,16 @@ import Kuliah from "./pages/Kuliah/Kuliah";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
-import TeknikInformatika from "./pages/TeknikInformatika/Teknikinformatika";
+import TeknikInformatika from "./pages/Prody/Teknikinformatika";
+import SistemInformasi from "./pages/Prody/SistemInformasi";
+import SistemKomputer from "./pages/Prody/SistemKomputer";
+import DesainKomunikasiVisual from "./pages/Prody/DesainKumunikasiVisual";
+import Akuntansi from "./pages/Prody/Akuntansi";
+import Management from "./pages/Prody/Management";
+import Bisnis from "./pages/Prody/Bisnis";
+import Hukum from "./pages/Prody/Hukum"
+
+
 
 function App() {
   return (
@@ -13,6 +22,15 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/ti" element={<TeknikInformatika />} />
+      <Route path="/Si" element={<SistemInformasi />} />
+      <Route path="/sk" element={<SistemKomputer />} />
+      <Route path="/dkv" element={<DesainKomunikasiVisual />} />
+      <Route path="/ak" element={<Akuntansi />} />
+      <Route path="/mm" element={<Management />} />
+      <Route path="/bsn" element={<Bisnis />} />
+      <Route path="/hk" element={<Hukum />} />
+      
+      
     </Routes>
   );
 }
