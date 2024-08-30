5e8c6c9505b9583bf7283129bc70fa1a5eddc679
Fitri Nur Azis <email@contoh.com>
2024-08-30 20:49:39 +0700

    fix/quote-home

--- src/components/Quote/Quote.jsx
+++ src/components/Quote/Quote.jsx
@@ -1,5 +1,32 @@
-function Quote() {
-  return <div>Quote</div>;
-}
+import { useState, useEffect } from "react";
+import { Typewriter } from "react-simple-typewriter";
+import quotes from "../../json/quotes.json";
+
+const Quote = () => {
+  const [quote, setQuote] = useState("");
+
+  useEffect(() => {
+    const quotesArray = quotes.quotes;
+
+    const getRandomQuote = () => {
+      const randomIndex = Math.floor(Math.random() * quotesArray.length);
+      return `"${quotesArray[randomIndex].text}"`;
+    };
+
+    const intervalId = setInterval(() => {
+      setQuote(getRandomQuote());
+    }, 5000);
+
+    setQuote(getRandomQuote());
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div className="px-4 pt-10 pb-20 text-lg text-center font-dmserif">
+      <Typewriter words={[quote]} loop={1} cursor />
+    </div>
+  );
+};
 
 export default Quote;
