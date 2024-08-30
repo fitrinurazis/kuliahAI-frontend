6f6133ccfa9900636943881083d9d96612bdb485
Fitri Nur Azis <email@contoh.com>
2024-08-30 22:40:57 +0700

    fix/memperbaiki halaman prody

--- src/pages/Jurusan/Jurusan.jsx
+++ src/pages/Jurusan/Jurusan.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import jurusanData from "../../json/jurusan.json";
 
 function Jurusan() {
   return (
@@ -7,47 +8,14 @@ function Jurusan() {
         Program Study
       </h1>
       <div className="flex justify-center gap-3 px-3">
-        <ul className="flex flex-col justify-center w-full gap-3 text-center">
-          <Link to="/ti">
-            <li className="p-3 text-xl font-semibold border-2 rounded-lg font-poppins border-sky-700">
-              <span>Teknik Informatika </span>
-            </li>
-          </Link>
-          <Link to="/sk">
-            <li className="p-3 text-xl font-semibold border-2 rounded-lg font-poppins border-sky-700">
-              <span>Sistem Komputer </span>
-            </li>
-          </Link>
-          <Link to="/si">
-            <li className="p-3 text-xl font-semibold border-2 rounded-lg font-poppins border-sky-700">
-              <span>Sistem Informasi </span>
-            </li>
-          </Link>
-          <Link to="/dkv">
-            <li className="p-3 text-xl font-semibold border-2 rounded-lg font-poppins border-sky-700">
-              <span>Desain Komunikasi Visual </span>
-            </li>
-          </Link>
-          <Link to="/ak">
-            <li className="p-3 text-xl font-semibold border-2 rounded-lg font-poppins border-sky-700">
-              <span>Akuntansi </span>
-            </li>
-          </Link>
-          <Link to="/mm">
-            <li className="p-3 text-xl font-semibold border-2 rounded-lg font-poppins border-sky-700">
-              <span>Management </span>
-            </li>
-          </Link>
-          <Link to="/bsn">
-            <li className="p-3 text-xl font-semibold border-2 rounded-lg font-poppins border-sky-700">
-              <span>Bisnis </span>
-            </li>
-          </Link>
-          <Link to="/hk">
-            <li className="p-3 text-xl font-semibold border-2 rounded-lg font-poppins border-sky-700">
-              <span>Hukum </span>
-            </li>
-          </Link>
+        <ul className="flex flex-col justify-center w-full gap-5 text-center">
+          {jurusanData.jurusan.map((jurusan) => (
+            <Link key={jurusan.id} to={jurusan.link}>
+              <li className="p-3 text-xl font-semibold border-2 rounded-lg font-poppins border-sky-700">
+                <span>{jurusan.text}</span>
+              </li>
+            </Link>
+          ))}
         </ul>
       </div>
     </div>
